ae3a78d62004f08af1885f9be9f0235b33f20dff
Judeh822 <jude.hataway.27@gmail.com>
2026-03-20 22:39:09 -0500

    Intro added to lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Jude Hataway" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hi everyone. My name is Jude Hataway, and I am a junior from Texas. I love programming video games and building electronics projects with Arduino. I also play the Euphonium in band and compete in UIL science competitions.")
 
 if __name__ == "__main__":
     main()
